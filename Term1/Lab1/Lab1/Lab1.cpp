3639a5a79a2ada1e7c4a9b2d30ee40f0094ceea9
Марк Москаленко <Mark.Moskalenko@cs.khpi.edu.ua>
2023-11-11 11:29:59 +0200

    Add .gitattributes, .gitignore, and README.md.

---
+++ Term1/Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+#include <cstdio>
+
+int main() {
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of short: %zu bytes\n", sizeof(short));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+
+    return 0;
+}
+=======
+int main() {
+    int intSize = sizeof(int);
+    int shortSize = sizeof(short);
+    int longSize = sizeof(long);
+    int charSize = sizeof(char);
+    int floatSize = sizeof(float);
+    int doubleSize = sizeof(double);
+
+    return 0;
+}
+>>>>>>> Add .gitattributes, .gitignore, and README.md. g
